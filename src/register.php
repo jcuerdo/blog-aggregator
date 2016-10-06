1f5bc3b1b1ab10586b21eba8518a5d185b1b0606
Jorge Cuerdo <jcuerdo@magento.com>
2016-10-06 17:29:45 +0200

    Reload cache

--- src/register.php
+++ src/register.php
@@ -4,7 +4,8 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
     'twig.options'		=> array(
         'cache'			=> '/tmp',
-    ),
+	'auto_reload' => true, 
+   ),
 ));
 
 $app->register(new Silex\Provider\UrlGeneratorServiceProvider());
