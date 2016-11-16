d05d8b732188ef417a93ebae3c04b3bf5b4e3b0c
Jorge Cuerdo Álvarez <jocual@gmail.com>
2016-11-16 01:15:34 +0100

    RSS add

--- src/rss.php
+++ src/rss.php
@@ -1,5 +1,10 @@
 <?php
 
 $app->rss = [
-
+'http://feeds.feedburner.com/pixelarme?format=xml',
+    'http://cabeseo.com/posts/feed',
+    'http://sentidoweb.com/feed',
+    'http://feeds.feedburner.com/unijimpe',
+    'http://feeds.feedburner.com/anieto2k/Xzqf?format=xml',
+    'http://artegami.com/feed/',
 ];
