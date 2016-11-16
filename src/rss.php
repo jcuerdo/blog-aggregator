f073867709fe7405b01bf5cbca83d1781cec8248
Jorge Cuerdo Álvarez <jocual@gmail.com>
2016-11-16 02:17:36 +0100

    Add rss

--- src/rss.php
+++ src/rss.php
@@ -22,5 +22,14 @@ $app->rss = [
     'http://pythonentresdias.blogspot.com/feeds/posts/default',
     'http://relopezbriega.github.io/atom.xml',
     'https://davidcapello.com/atom.xml',
+    'http://staging.solidgear.es/rss.xml',
+    'http://blog.chuidiang.com/feed/',
+    'https://www.paradigmadigital.com/feed/',
+    'https://josepablosarco.wordpress.com/feed/',
+    'http://www.scrummanager.net/blog/feed/',
+    'http://patronesdediseno.blogspot.com/feeds/posts/default',
+    'https://devexperto.com/feed/',
+    'http://elblogdepicodev.blogspot.com/feeds/posts/default?alt=rss'
+
 
 ];
