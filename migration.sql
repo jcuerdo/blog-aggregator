1de1bc893a9d5ba325a52c01e71c6e41e200075f
Jorge Cuerdo Álvarez <jocual@gmail.com>
2019-12-09 14:08:50 +0100

    Update migration.sql

--- migration.sql
+++ migration.sql
@@ -1,15 +1,18 @@
-CREATE TABLE IF NOT EXISTS post (
-  title varchar(240) DEFAULT NULL,
-  slag varchar(240) NOT NULL DEFAULT '',
-  date timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  link varchar(512) DEFAULT NULL,
-  content text,
-  PRIMARY KEY (slag),
-  KEY slag (slag)
+CREATE TABLE `post` (
+  `title` varchar(240) DEFAULT NULL,
+  `slag` varchar(240) NOT NULL DEFAULT '',
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `link` varchar(512) DEFAULT NULL,
+  `content` text,
+  `image` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`slag`),
+  KEY `slag` (`slag`),
+  KEY `DateIndex` (`date`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
-CREATE TABLE IF NOT EXISTS rss (
-  url varchar(300) DEFAULT NULL,
-  UNIQUE (url)
+
+CREATE TABLE `rss` (
+  `url` varchar(300) DEFAULT NULL,
+  UNIQUE KEY `url` (`url`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
