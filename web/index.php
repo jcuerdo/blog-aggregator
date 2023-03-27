8e0a47d7e48f2a49222e852dd29f09401881418b
Jorge Cuerdo Álvarez <jocual@gmail.com>
2023-03-27 10:16:41 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -6,6 +6,7 @@ $app->error(function (\Exception $e, $code) use($app) {
     $app['monolog']->error($e->getMessage(), $e->getTrace());
     switch ($code) {
         case 404:
+            $app->redirect($app["url_generator"]->generate("index"));
             $message = $app['twig']->render('error/404.twig');
             break;
         default:
